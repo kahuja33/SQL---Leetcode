60bc33bf599d1212babf02f4799a2ff64e4de4c7
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-23 01:21:47 +0530

    Time: 1006 ms (68.77%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-With ce1 as (
-Select a.project_id,a.employee_id,b.name,b.experience_years from Project a inner join Employee b on a.employee_id=b.employee_id order by a.project_id,a.employee_id )
-Select project_id,round(avg(experience_years),2) as average_years from ce1 group by project_id
+#With ce1 as (
+#Select a.project_id,a.employee_id,b.name,b.experience_years from Project a inner join Employee b on a.employee_id=b.employee_id order by a.project_id,a.employee_id )
+#Select project_id,round(avg(experience_years),2) as average_years from ce1 group by project_id
+
+Select project_id,round(average_years,2) as average_years from(Select a.project_id,avg(experience_years) over (partition by project_id) as average_years,ROW_NUMBER() OVER (PARTITION BY a.project_id ORDER BY a.project_id) as row_num from Project a inner join Employee b on a.employee_id=b.employee_id) as grouped_experience where row_num=1
