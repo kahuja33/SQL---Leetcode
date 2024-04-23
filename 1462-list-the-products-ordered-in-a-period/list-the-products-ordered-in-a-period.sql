1d1899a362e9035bb8089bd5115a1777cc9a8faa
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-23 21:33:26 +0530

    Time: 2480 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,1 +1,3 @@
-Select a.product_name,  sum(b.unit) as Unit from products a inner join orders b on a.product_id=b.product_id where date_format(b.order_date,"%Y-%m")="2020-02" group by product_name having sum(b.unit)>=100
+#Select a.product_name,  sum(b.unit) as Unit from products a inner join orders b on a.product_id=b.product_id where date_format(b.order_date,"%Y-%m")="2020-02" group by product_name having sum(b.unit)>=100
+
+Select product_name,  sum(unit) as Unit from (select a.product_name as product_name, b.order_date as order_date,b.unit as unit from products a inner join orders b on a.product_id=b.product_id where date_format(b.order_date,"%Y-%m")="2020-02") as te1 group by product_name having sum(unit)>=100
