7dc87beebb3fa37a93a4a9d716ffb40203e1317a
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-24 01:06:28 +0530

    Time: 2483 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
 #Select ifnull(num,"null") as num from mynumbers group by num having count(num)=1 order by num desc limit 1
 
-select
-case when count(num)=1 then num else null end as num
-from MyNumbers
-group by num
-order by num desc limit 1
+select case when count(num)=1 then num else null end as num from MyNumbers group by num order by num desc limit 1
