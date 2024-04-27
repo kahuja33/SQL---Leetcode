431fe1b991dec0fdb4a52b8087bc4e7ca5418dfc
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-27 20:09:39 +0530

    Time: 2102 ms (5.09%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-#select (sum(rw)-sum(dif))/sum(rw)*100 as immediate_percentage from(
-#select delivery_id, customer_id,order_date,customer_pref_delivery_date, rank() over(partition by customer_id order by order_date) as rw,datediff(customer_pref_delivery_date,order_date) as dif from delivery) as temp where rw=1  
+
 select round(sum(case when dif=0 then 1 else 0 end)*100/sum(rw),2) as immediate_percentage from (
 select delivery_id, customer_id,order_date,customer_pref_delivery_date, rank() over(partition by customer_id order by order_date) as rw,datediff(customer_pref_delivery_date,order_date) as dif from delivery) as temp where rw=1 
