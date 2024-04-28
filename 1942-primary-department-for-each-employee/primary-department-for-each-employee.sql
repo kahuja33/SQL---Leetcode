fb35d7623676aee5364da522e992196338ab7121
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-29 00:17:33 +0530

    Time: 1086 ms (55.89%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-#select employee_id,department_id from (
-#select *,rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,COUNT(EMPLOYEE_ID)over (partition by   employee_id) as r2 from employee) as temp where (r1=1 and primary_flag="Y") or (r1=1 and r2=1 and primary_flag="N")
+select employee_id,department_id from (
+select *,rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,COUNT(EMPLOYEE_ID)over (partition by   employee_id) as r2 from employee) as temp where (r1=1 and primary_flag="Y") or (r1=1 and r2=1 and primary_flag="N")
 
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y' #it eliminates duplicates and gives the 'Y' assigned emoplyee_id only
-UNION 
-SELECT employee_id, department_id
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(employee_id) = 1;
+#SELECT employee_id, department_id FROM Employee WHERE primary_flag = 'Y' UNION SELECT employee_id, department_id FROM Employee GROUP BY employee_id HAVING COUNT(employee_id) = 1;
 
