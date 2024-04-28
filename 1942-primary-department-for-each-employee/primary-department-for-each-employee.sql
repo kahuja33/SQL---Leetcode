af10f98c5071f6d747670c4960ad1345c56c5c61
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-29 00:16:41 +0530

    Time: 915 ms (89.95%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-select employee_id,department_id from (
-select *,rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,COUNT(EMPLOYEE_ID)over (partition by   employee_id) as r2 from employee) as temp where (r1=1 and primary_flag="Y") or (r1=1 and r2=1 and primary_flag="N")
+#select employee_id,department_id from (
+#select *,rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,COUNT(EMPLOYEE_ID)over (partition by   employee_id) as r2 from employee) as temp where (r1=1 and primary_flag="Y") or (r1=1 and r2=1 and primary_flag="N")
 
-#select *,dense_rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,case when  from employee
-#select employee_id,primary_flag,sum(case when primary_flag='Y' then 1 else 0 end) from employee group by employee_id,primary_flag
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y' #it eliminates duplicates and gives the 'Y' assigned emoplyee_id only
+UNION 
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1;
 
-#select *,rank() over(partition by employee_id order by case when primary_flag="Y" then 1 else 2 end) as r1,COUNT(EMPLOYEE_ID)over (partition by   employee_id) as r2 from employee
