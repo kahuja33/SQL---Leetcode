5c7b77b3b8e4b7ecf65931bfe325145ce0ef24a6
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-04-30 21:30:17 +0530

    Time: 2115 ms (34.05%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
 
-#with cte as(select count(user_id) from users)
-#select contest_id,round(count(user_id)*100/(select * from cte),2) as percentage from(
-#Select a.user_id,a.user_name,b.contest_id from Users a inner join Register b on a.user_id=b.user_id) as temp group by contest_id order by percentage desc,contest_id
-SELECT contest_id,
-ROUND(COUNT(DISTINCT user_id) * 100 / (SELECT COUNT(user_id) FROM Users), 2) as percentage
-FROM Register
-GROUP BY contest_id
-ORDER BY percentage desc, contest_id
+with cte as(select count(user_id) from users)
+select contest_id,round(count(user_id)*100/(select * from cte),2) as percentage from(
+Select a.user_id,a.user_name,b.contest_id from Users a inner join Register b on a.user_id=b.user_id) as temp group by contest_id order by percentage desc,contest_id
