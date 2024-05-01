e526706c57ebcfdcb3ad5501c33a60bff8bb0e38
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-05-01 23:57:51 +0530

    Time: 1304 ms (85.49%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-#Select a.product_id,case when b.product_id is null then 0 else round(sum(price*units)/sum(units),2) end as average_price from prices a inner join unitssold b on a.product_id=b.product_id and b.purchase_date<=a.end_Date and b.purchase_date>=a.start_Date group by a.product_id
+#Select a.product_id,case when b.product_id is null then 0 else round(sum(price*units)/sum(units),2) end as average_price from prices a left join unitssold b on a.product_id=b.product_id and b.purchase_date<=a.end_Date and b.purchase_date>=a.start_Date group by a.product_id
 
 Select a.product_id,coalesce(round(sum(price*units)/sum(units),2),0) as average_price from prices a left join unitssold b on a.product_id=b.product_id and b.purchase_date<=a.end_Date and b.purchase_date>=a.start_Date group by a.product_id
