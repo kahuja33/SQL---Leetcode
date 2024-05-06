fa6ac3b82a238d74128d86342d65332e27b2ea0f
kahuja33 <167449589+kahuja33@users.noreply.github.com>
2024-05-07 03:12:09 +0530

    Time: 880 ms (65.90%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,11 +1,3 @@
-#part 1: Identify all the distinct Product Id
-#part 2: Find the latest price of the product till the date 16-8-2019
-#part 3: if the product has no new price before or on 16-8-2019, then assign $10 as an original price
-#part 4: either use minus operation or union to combine both of the tables
-
-#part 2 - part 1
-#remaining part assign as $10 and do the Union
-
 With cte as (select product_id from products group by product_id),
 cte2 as (Select product_id from(
 Select product_id,new_price,rank() over(partition by product_id order by change_date desc)
